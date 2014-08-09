9fe49d00086ea01256526b1806c9e89aca5db928
Andrey Belchikov <belchikovs@gmail.com>
2014-08-09 17:30:36 +0400

    Restored Messages tests

--- Bandwidth.Net.Tests/Clients/MessagesTests.cs
+++ Bandwidth.Net.Tests/Clients/MessagesTests.cs
@@ -1,9 +1,5 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Net.Http.Fakes;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Bandwidth.Net.Data;
-using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bandwidth.Net.Tests.Clients
@@ -14,55 +10,50 @@ namespace Bandwidth.Net.Tests.Clients
         [TestMethod]
         public void SendTest()
         {
-            using (ShimsContext.Create())
+            var message = new Message
             {
-                ShimHttpClient.AllInstances.PostAsyncStringHttpContent = (c, url, content) =>
-                {
-                    Assert.AreEqual(string.Format("users/{0}/messages", Helper.UserId), url);
-                    var message = Helper.ParseJsonContent<Message>(content).Result;
-                    Assert.AreEqual("From", message.From);
-                    Assert.AreEqual("To", message.To);
-                    var response = new HttpResponseMessage(HttpStatusCode.Created);
-                    response.Headers.Add("Location", string.Format("/v1/users/{0}/messages/1", Helper.UserId));
-                    return Task.Run(() => response);
-                };
+                From = "From",
+                To = "To",
+                Text = "Text"
+            };
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "POST",
+                EstimatedPathAndQuery = string.Format("/v1/users/{0}/messages", Helper.UserId),
+                EstimatedContent = Helper.ToJsonString(message),
+                HeadersToSend = new Dictionary<string, string> { { "Location", string.Format("/v1/users/{0}/messages/1", Helper.UserId) } }
+            }))
+            {
                 using (var client = Helper.CreateClient())
                 {
-                    var id = client.Messages.Send(new Message
-                    {
-                        From = "From",
-                        To = "To"
-                    }).Result;
+                    var id = client.Messages.Send(message).Result;
+                    if (server.Error != null) throw server.Error;
                     Assert.AreEqual("1", id);
                 }
             }
         }
 
-        
-
         [TestMethod]
         public void GetTest()
         {
-            using (ShimsContext.Create())
+            var message = new Message
             {
-                var message = new Message
-                {
-                    Id = "1",
-                    From = "From",
-                    To = "To"
-                };
-                ShimHttpClient.AllInstances.GetAsyncString = (c, url) =>
-                {
-                    Assert.AreEqual(string.Format("users/{0}/messages/1", Helper.UserId), url);
-                    var response = new HttpResponseMessage(HttpStatusCode.OK)
-                    {
-                        Content = Helper.CreateJsonContent(message)
-                    };
-                    return Task.Run(() => response);
-                };
+                Id = "1",
+                From = "From",
+                To = "To",
+                Text = "Text"
+            };
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "GET",
+                EstimatedPathAndQuery = string.Format("/v1/users/{0}/messages/1", Helper.UserId),
+                ContentToSend = Helper.CreateJsonContent(message)
+            }))
+            {
                 using (var client = Helper.CreateClient())
                 {
                     var result = client.Messages.Get("1").Result;
+                    if (server.Error != null) throw server.Error;
                     Helper.AssertObjects(message, result);
                 }
             }
@@ -71,35 +62,33 @@ namespace Bandwidth.Net.Tests.Clients
         [TestMethod]
         public void GetAllTest()
         {
-            using (ShimsContext.Create())
+            var messages = new[]{
+                new Message
+                {
+                    Id = "1",
+                    From = "From",
+                    To = "To",
+                    Text = "Text"
+                },
+                new Message
+                {
+                    Id = "2",
+                    From = "From",
+                    To = "To",
+                    Text = "Text"
+                }
+            };
+            using (var server = new HttpServer(new RequestHandler
             {
-                var messages = new[]
-                {
-                    new Message
-                    {
-                        Id = "1",
-                        From = "From",
-                        To = "To"
-                    },
-                    new Message
-                    {
-                        Id = "2",
-                        From = "From2",
-                        To = "To2"
-                    }
-                };
-                ShimHttpClient.AllInstances.GetAsyncString = (c, url) =>
-                {
-                    Assert.AreEqual(string.Format("users/{0}/messages", Helper.UserId), url);
-                    var response = new HttpResponseMessage(HttpStatusCode.OK)
-                    {
-                        Content = Helper.CreateJsonContent(messages)
-                    };
-                    return Task.Run(() => response);
-                };
+                EstimatedMethod = "GET",
+                EstimatedPathAndQuery = string.Format("/v1/users/{0}/messages", Helper.UserId),
+                ContentToSend = Helper.CreateJsonContent(messages)
+            }))
+            {
                 using (var client = Helper.CreateClient())
                 {
                     var result = client.Messages.GetAll().Result;
+                    if (server.Error != null) throw server.Error;
                     Assert.AreEqual(2, result.Length);
                     Helper.AssertObjects(messages[0], result[0]);
                     Helper.AssertObjects(messages[1], result[1]);
