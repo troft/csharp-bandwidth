06a816779284daad381322396088f3f30a18c0f6
Scott Barstow <sbarstow@gmail.com>
2014-11-03 17:58:08 -0500

    added SmsEvent props and basic unit tests

--- Bandwidth.Net/Model/SmsEvent.cs
+++ Bandwidth.Net/Model/SmsEvent.cs
@@ -2,5 +2,13 @@
 {
     public class SmsEvent : BaseEvent
     {
+        public string Direction { get; set; }
+        public string MessageId { get; set; }
+        public string MessageUrl { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Text { get; set; }
+        public string ApplicationId { get; set; }
+        public string State { get; set; }
     }
 }
