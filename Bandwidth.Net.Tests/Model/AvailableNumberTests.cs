22df3bc70c344d56a2954303bfe996d52b25fcb6
Scott Barstow <sbarstow@gmail.com>
2014-11-04 16:18:19 -0500

    added availableNumber api and initial tests

---
+++ Bandwidth.Net.Tests/Model/AvailableNumberTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bandwidth.Net.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bandwidth.Net.Tests.Model
+{
+    [TestClass]
+    public class AvailableNumberTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Helper.SetEnvironmetVariables();
+        }
+
+        [TestMethod]
+        public void TestSearchLocal()
+        {
+            var availableNumbers = new[]
+                {
+                    new AvailableNumber
+                        {
+                            Number = "1",
+                            City = "City1",
+                            Lata = "Lata1",
+                            NationalNumber = "NationalNumber1",
+                            PatternMatch = "PatternMatch1",
+                            Price = 0.01m,
+                            RateCenter = "RateCenter1",
+                            State = "State1",
+
+                        },
+                    new AvailableNumber
+                        {
+                            Number = "2",
+                            City = "City2",
+                            Lata = "Lata2",
+                            NationalNumber = "NationalNumber2",
+                            PatternMatch = "PatternMatch2",
+                            Price = 0.01m,
+                            RateCenter = "RateCenter2",
+                            State = "State2",
+
+                        }
+                };
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "GET",
+                EstimatedPathAndQuery = "/v1/availableNumbers/local",
+                ContentToSend = Helper.CreateJsonContent(availableNumbers)
+            }))
+            {
+                var client = Helper.CreateClient();
+                var result = AvailableNumber.SearchLocal(client).Result;
+                if (server.Error != null) throw server.Error;
+                Assert.AreEqual(2, result.Length);
+                Helper.AssertObjects(availableNumbers[0], result[0]);
+                Helper.AssertObjects(availableNumbers[1], result[1]);
+            }
+        }
+
+        [TestMethod]
+        public void TestSearchTollFree()
+        {
+            var availableNumbers = new[]
+                {
+                    new AvailableNumber
+                        {
+                            Number = "1",
+                            NationalNumber = "NationalNumber1",
+                            PatternMatch = "PatternMatch1",
+                            Price = 0.01m,
+                        },
+                    new AvailableNumber
+                        {
+                            Number = "2",
+                            NationalNumber = "NationalNumber2",
+                            PatternMatch = "PatternMatch2",
+                            Price = 0.01m,
+                        }
+                };
+
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "GET",
+                EstimatedPathAndQuery = "/v1/availableNumbers/tollFree",
+                ContentToSend = Helper.CreateJsonContent(availableNumbers)
+            }))
+            {
+                var client = Helper.CreateClient();
+                var result = AvailableNumber.SearchTollFree(client).Result;
+                if (server.Error != null) throw server.Error;
+                Assert.AreEqual(2, result.Length);
+                Helper.AssertObjects(availableNumbers[0], result[0]);
+                Helper.AssertObjects(availableNumbers[1], result[1]);
+            }
+        }
+    }
+}
---
+++ Bandwidth.Net/Model/AvailableNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandwidth.Net.Model
+{
+    public class AvailableNumber
+    {
+        private const string AvailableNumbersTollFreePath = "availableNumbers/tollFree";
+        private const string AvailableNumbersLocalPath = "availableNumbers/local";
+
+        public static async Task<AvailableNumber[]> SearchTollFree(Client client, Dictionary<string, object> query = null)
+        {
+            var availableNumbers = await client.MakeGetRequest<AvailableNumber[]>(AvailableNumbersTollFreePath, query);
+            return availableNumbers;
+        }
+
+        public static async Task<AvailableNumber[]> SearchLocal(Client client, Dictionary<string, object> query = null)
+        {
+            var availableNumbers = await client.MakeGetRequest<AvailableNumber[]>(AvailableNumbersLocalPath, query);
+            return availableNumbers;
+        }
+
+        public string Number { get; set; }
+        public string NationalNumber { get; set; }
+        public string PatternMatch { get; set; }
+        public string City { get; set; }
+        public string Lata { get; set; }
+        public string RateCenter { get; set; }
+        public string State { get; set; }
+        public decimal Price { get; set; }
+    }
+}
