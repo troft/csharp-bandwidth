80a91d48cbcc8a02abb6ab51cc90f151ff8b38f3
Andrey Belchikov <belchikovs@gmail.com>
2014-11-10 17:52:51 +0300

    Added ability to use default client by new instance of Models

--- Bandwidth.Net/BaseModel.cs
+++ Bandwidth.Net/BaseModel.cs
@@ -2,8 +2,27 @@
 {
     public class BaseModel
     {
+        private Client _client;
         public string Id { get; set; }
 
-        internal Client Client;
+        internal Client Client
+        {
+            get
+            {
+#if !PCL
+                if (_client == null)
+                {
+                    _client = Client.GetInstance();
+                }
+#endif
+                return _client;
+            }
+            set { _client = value; }
+        }
+
+        public void SetClient(Client client)
+        {
+            _client = client;
+        }
     }
 }
