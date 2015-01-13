3f042f6104297de55b764de0201f6d10045a3a1c
Andrey Belchikov <belchikovs@gmail.com>
2015-01-13 13:48:26 +0300

    Added xml docs to BaseConferenceEvent, ConferenceEvent and CallEvent

--- Bandwidth.Net/Model/BaseConferenceEvent.cs
+++ Bandwidth.Net/Model/BaseConferenceEvent.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bandwidth.Net.Model
+﻿namespace Bandwidth.Net.Model
 {
+    /// <summary>
+    /// Base class for all conference events
+    /// </summary>
     public abstract class BaseConferenceEvent : BaseEvent
     {
+        /// <summary>
+        /// The unique identifier of the conference
+        /// </summary>
         public string ConferenceId { get; set; }
+        
+        /// <summary>
+        /// Uri of the condefence
+        /// </summary>
         public string ConferenceUri { get; set; }
+        
+        /// <summary>
+        /// Status
+        /// </summary>
         public string Status { get; set; }
     }
 }
--- Bandwidth.Net/Model/ConferenceEvent.cs
+++ Bandwidth.Net/Model/ConferenceEvent.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bandwidth.Net.Model
 {
+    /// <summary>
+    /// Conference event
+    /// </summary>
+    /// <seealso href="https://catapult.inetwork.com/docs/callback-events/conference-event/"/>
     public class ConferenceEvent : BaseConferenceEvent
     {
+        /// <summary>
+        /// Time of creation of the conference
+        /// </summary>
         public DateTime CreatedTime { get; set; }
+        
+        /// <summary>
+        /// Time of completion of the conference
+        /// </summary>
         public DateTime CompletedTime { get; set; }
     }
 }
