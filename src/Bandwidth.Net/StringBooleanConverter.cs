8d40b0f4109598919321151983efa4fccae7522f
Unknown <belchikovs@gmail.com>
2016-08-17 11:04:27 +0300

    Added tests for StringBooleanConverter

--- src/Bandwidth.Net/StringBooleanConverter.cs
+++ src/Bandwidth.Net/StringBooleanConverter.cs
@@ -9,18 +9,26 @@ namespace Bandwidth.Net
 
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
-      writer.WriteValue(value as string);
+      writer.WriteValue(value?.ToString().ToLowerInvariant());
     }
 
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
       var value = (reader.Value)?.ToString();
-      return string.CompareOrdinal(value, "true") == 0;
+      if (string.CompareOrdinal(value, "true") == 0)
+      {
+        return true;
+      }
+      if (string.CompareOrdinal(value, "false") == 0)
+      {
+        return false;
+      }
+      return (objectType == typeof (bool?)) ? null : (object) default(bool);
     }
 
     public override bool CanConvert(Type objectType)
     {
-      return objectType == typeof (string) || objectType == typeof (bool) || objectType == typeof(bool?);
+      return objectType == typeof (bool) || objectType == typeof(bool?);
     }
   }
 }
---
+++ test/Bandwidth.Net.Test/StringBooleanConverterTests.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Bandwidth.Net.Test
+{
+  public class StringBooleanConverterTests
+  {
+
+    [Fact]
+    public void TestReadJson()
+    {
+      var item = JsonConvert.DeserializeObject<TestItem>("{\"Item1\":\"true\", \"Item2\":\"true\"}");
+      Assert.True(item.Item1);
+      Assert.True(item.Item2);
+    }
+
+    [Fact]
+    public void TestReadJson2()
+    {
+      var item = JsonConvert.DeserializeObject<TestItem>("{\"Item1\":\"false\", \"Item2\":\"false\"}");
+      Assert.False(item.Item1);
+      Assert.False(item.Item2);
+    }
+
+    [Fact]
+    public void TestReadJson3()
+    {
+      var item = JsonConvert.DeserializeObject<TestItem>("{}");
+      Assert.False(item.Item1);
+      Assert.Null(item.Item2);
+    }
+
+    [Fact]
+    public void TestWriteJson()
+    {
+      var item = new TestItem
+      {
+        Item1 = true,
+        Item2 = true
+      };
+      var json = JsonConvert.SerializeObject(item);
+      Assert.Equal("{\"Item1\":\"true\",\"Item2\":\"true\"}", json);
+    }
+
+    [Fact]
+    public void TestWriteJson2()
+    {
+      var item = new TestItem
+      {
+        Item1 = false,
+        Item2 = false
+      };
+      var json = JsonConvert.SerializeObject(item);
+      Assert.Equal("{\"Item1\":\"false\",\"Item2\":\"false\"}", json);
+    }
+
+    [Fact]
+    public void TestWriteJson3()
+    {
+      var item = new TestItem();
+      var json = JsonConvert.SerializeObject(item);
+      Assert.Equal("{\"Item1\":\"false\",\"Item2\":null}", json);
+    }
+
+
+    public class TestItem
+    {
+      [JsonConverter(typeof (StringBooleanConverter))]
+      public bool Item1 { get; set; }
+      
+      [JsonConverter(typeof (StringBooleanConverter))]
+      public bool? Item2 { get; set; }
+    }
+  }
+}
