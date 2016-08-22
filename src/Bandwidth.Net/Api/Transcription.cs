6d30c0574495372695e705137e82c4db620a7fd7
avbel <belchikovs@gmail.com>
2016-08-22 23:09:13 +0300

    Added Transcription API (#44)

--- src/Bandwidth.Net/Api/Transcription.cs
+++ src/Bandwidth.Net/Api/Transcription.cs
@@ -1,26 +1,166 @@
-﻿namespace Bandwidth.Net.Api
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bandwidth.Net.Api
 {
   /// <summary>
-  /// Access to Transcription Api
+  ///   Access to Transcription Api
   /// </summary>
   public interface ITranscription
   {
+    /// <summary>
+    ///   Get a list of transcriptions
+    /// </summary>
+    /// <param name="recordingId">Id of the recording</param>
+    /// <param name="query">Optional query parameters</param>
+    /// <param name="cancellationToken">>Optional token to cancel async operation</param>
+    /// <returns>Collection with <see cref="Transcription" /> instances</returns>
+    /// <example>
+    ///   <code>
+    /// var transcriptions = client.Transcription.List("recordingId"); 
+    /// </code>
+    /// </example>
+    IEnumerable<Transcription> List(string recordingId, TranscriptionQuery query = null,
+      CancellationToken? cancellationToken = null);
+
+    /// <summary>
+    ///   Create a transcription.
+    /// </summary>
+    /// <param name="recordingId">Id of the recording</param>
+    /// <param name="cancellationToken">Optional token to cancel async operation</param>
+    /// <returns>Instance of created transcription</returns>
+    /// <example>
+    ///   <code>
+    /// var transcription = await client.Transcription.CreateAsync("recordingId");
+    /// </code>
+    /// </example>
+    Task<ILazyInstance<Transcription>> CreateAsync(string recordingId, CancellationToken? cancellationToken = null);
+
+
+    /// <summary>
+    ///   Get information about a transcription
+    /// </summary>
+    /// <param name="recordingId">Id of the recording</param>
+    /// <param name="transcriptionId">Id of transcription to get</param>
+    /// <param name="cancellationToken">Optional token to cancel async operation</param>
+    /// <returns>Task with <see cref="Transcription" />Transcription instance</returns>
+    /// <example>
+    ///   <code>
+    /// var transcription = await client.Transcription.GetAsync("recordingId", "transcriptionId");
+    /// </code>
+    /// </example>
+    Task<Transcription> GetAsync(string recordingId, string transcriptionId, CancellationToken? cancellationToken = null);
   }
 
-  //TODO implement
   internal class TranscriptionApi : ApiBase, ITranscription
   {
+    public IEnumerable<Transcription> List(string recordingId, TranscriptionQuery query = null,
+      CancellationToken? cancellationToken = null)
+    {
+      return new LazyEnumerable<Transcription>(Client,
+        () =>
+          Client.MakeJsonRequestAsync(HttpMethod.Get, $"/users/{Client.UserId}/recordings/{recordingId}/transcriptions",
+            cancellationToken, query));
+    }
 
+    public async Task<ILazyInstance<Transcription>> CreateAsync(string recordingId,
+      CancellationToken? cancellationToken = null)
+    {
+      var id =
+        await
+          Client.MakePostJsonRequestAsync($"/users/{Client.UserId}/recordings/{recordingId}/transcriptions",
+            cancellationToken, new object());
+      return new LazyInstance<Transcription>(id, () => GetAsync(recordingId, id));
+    }
+
+    public Task<Transcription> GetAsync(string recordingId, string transcriptionId,
+      CancellationToken? cancellationToken = null)
+    {
+      return Client.MakeJsonRequestAsync<Transcription>(HttpMethod.Get,
+        $"/users/{Client.UserId}/bridges/recordings/{recordingId}/transcriptions/{transcriptionId}", cancellationToken);
+    }
   }
 
   /// <summary>
-  /// TODO implement
+  ///   Transcription data
   /// </summary>
   public class Transcription
   {
     /// <summary>
-    /// TODO
+    ///   The unique id of the transcriptions resource.
     /// </summary>
     public string Id { get; set; }
+
+    /// <summary>
+    /// The state of the transcription
+    /// </summary>
+    public TranscriptionStates State { get; set; }
+
+    /// <summary>
+    /// The transcribed text.
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// The date/time the transcription resource was created
+    /// </summary>
+    public DateTime Time { get; set; }
+
+    /// <summary>
+    /// The seconds between activeTime and endTime for the recording; this is the time that is going to be used to charge the resource.
+    /// </summary>
+    public int ChargeableDuration { get; set; }
+
+    /// <summary>
+    /// The size of the transcribed text. If the text is longer than 1000 characters it will be cropped; the full text can be retrieved from the url available at textUrl property.
+    /// </summary>
+    public int TextSize { get; set; }
+
+    /// <summary>
+    /// An url to the full text; this property is available regardless the textSize.
+    /// </summary>
+    public string TextUrl { get; set; }
+
+    /// <summary>
+    /// Media name of full text file
+    /// </summary>
+    public string TextMediaName => TextUrl.Split('/').Last();
+  }
+
+  /// <summary>
+  ///   Query to get transcriptions
+  /// </summary>
+  public class TranscriptionQuery
+  {
+    /// <summary>
+    ///   Used for pagination to indicate the size of each page requested for querying a list of transcriptions. If no value is
+    ///   specified the default value is 25 (maximum value 1000).
+    /// </summary>
+    public int? Size { get; set; }
+  }
+
+  /// <summary>
+  /// States of transcription
+  /// </summary>
+  public enum TranscriptionStates
+  {
+    /// <summary>
+    /// Transcribing
+    /// </summary>
+    Transcribing,
+
+    /// <summary>
+    /// Completed
+    /// </summary>
+    Completed,
+
+    /// <summary>
+    /// Error
+    /// </summary>
+    Error
   }
 }
---
+++ test/Bandwidth.Net.Test/Api/TranscriptionTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Bandwidth.Net.Api;
+using LightMock;
+using Xunit;
+
+namespace Bandwidth.Net.Test.Api
+{
+  public class TranscriptionTest
+  {
+    [Fact]
+    public void TestList()
+    {
+      var response = new HttpResponseMessage
+      {
+        Content =
+          new JsonContent($"[{Helpers.GetJsonResourse("Transcription")}]")
+      };
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidListRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(response));
+      var api = Helpers.GetClient(context).Transcription;
+      var transcriptions = api.List("recordingId");
+      ValidateTranscription(transcriptions.First());
+    }
+
+    [Fact]
+    public async void TestCreate()
+    {
+      var response = new HttpResponseMessage(HttpStatusCode.Created);
+      response.Headers.Location = new Uri("http://localhost/path/transcriptionId");
+      var getResponse = new HttpResponseMessage
+      {
+        Content = Helpers.GetJsonContent("Transcription")
+      };
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidCreateRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(response));
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidGetRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(getResponse));
+      var api = Helpers.GetClient(context).Transcription;
+      var transcription = await api.CreateAsync("recordingId");
+      context.Assert(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidGetRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null), Invoked.Never);
+      Assert.Equal("transcriptionId", transcription.Id);
+      ValidateTranscription(transcription.Instance);
+      context.Assert(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidGetRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null), Invoked.Once);
+    }
+
+    [Fact]
+    public async void TestGet()
+    {
+      var response = new HttpResponseMessage
+      {
+        Content = Helpers.GetJsonContent("Transcription")
+      };
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidGetRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(response));
+      var api = Helpers.GetClient(context).Transcription;
+      var transcription = await api.GetAsync("recordingId", "transcriptionId");
+      ValidateTranscription(transcription);
+    }
+
+
+    public static bool IsValidListRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Get && request.RequestUri.PathAndQuery == "/v1/users/userId/recordings/recordingId/transcriptions";
+    }
+
+    public static bool IsValidCreateRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Post && request.RequestUri.PathAndQuery == "/v1/users/userId/recordings/recordingId/transcriptions";
+    }
+
+    public static bool IsValidGetRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Get && request.RequestUri.PathAndQuery == "/v1/users/userId/bridges/recordings/recordingId/transcriptions/transcriptionId";
+    }
+
+    private static void ValidateTranscription(Transcription item)
+    {
+      Assert.Equal("transcriptionId", item.Id);
+      Assert.Equal(TranscriptionStates.Completed, item.State);
+    }
+  }
+}
