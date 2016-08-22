52a1a47f5c30712430de3b4cc5992704c9535cc0
avbel <belchikovs@gmail.com>
2016-08-22 23:08:42 +0300

    Added Recording API (#43)

--- src/Bandwidth.Net/Api/Recording.cs
+++ src/Bandwidth.Net/Api/Recording.cs
@@ -1,4 +1,11 @@
-﻿namespace Bandwidth.Net.Api
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bandwidth.Net.Api
 {
 
   /// <summary>
@@ -6,21 +13,131 @@
   /// </summary>
   public interface IRecording
   {
+    /// <summary>
+    ///   Get a list of recordings
+    /// </summary>
+    /// <param name="query">Optional query parameters</param>
+    /// <param name="cancellationToken">>Optional token to cancel async operation</param>
+    /// <returns>Collection with <see cref="Recording" /> instances</returns>
+    /// <example>
+    ///   <code>
+    /// var recordings = client.Recording.List(); 
+    /// </code>
+    /// </example>
+    IEnumerable<Recording> List(RecordingQuery query = null,
+      CancellationToken? cancellationToken = null);
+
+    /// <summary>
+    ///   Get information about a recording
+    /// </summary>
+    /// <param name="recordingId">Id of recording to get</param>
+    /// <param name="cancellationToken">Optional token to cancel async operation</param>
+    /// <returns>Task with <see cref="Recording" />Recording instance</returns>
+    /// <example>
+    ///   <code>
+    /// var recording = await client.Recording.GetAsync("recordingId");
+    /// </code>
+    /// </example>
+    Task<Recording> GetAsync(string recordingId, CancellationToken? cancellationToken = null);
+
   }
 
-  //TODO implement
   internal class RecordingApi : ApiBase, IRecording
   {
+    public IEnumerable<Recording> List(RecordingQuery query = null, CancellationToken? cancellationToken = null)
+    {
+      return new LazyEnumerable<Recording>(Client,
+        () =>
+          Client.MakeJsonRequestAsync(HttpMethod.Get, $"/users/{Client.UserId}/recordings", cancellationToken, query));
+    }
 
+    public Task<Recording> GetAsync(string recordingId, CancellationToken? cancellationToken = null)
+    {
+      return Client.MakeJsonRequestAsync<Recording>(HttpMethod.Get,
+        $"/users/{Client.UserId}/recordings/{recordingId}", cancellationToken);
+    }
   }
   /// <summary>
-  /// TODO implement
+  /// Recording data
   /// </summary>
   public class Recording
   {
     /// <summary>
-    /// TODO
+    /// The unique id of the recordings resource.
     /// </summary>
     public string Id { get; set; }
+
+    /// <summary>
+    /// Date/time when the recording started.
+    /// </summary>
+    public DateTime StartTime { get; set; }
+
+    /// <summary>
+    /// Date/time when the recording ended. 
+    /// </summary>
+    public DateTime EndTime { get; set; }
+
+    /// <summary>
+    /// The complete URL to the call resource this recording is associated with.
+    /// </summary>
+    public string Call { get; set; }
+
+    /// <summary>
+    /// Id of associated call
+    /// </summary>
+    public string CallId => Call.Split('/').Last();
+
+    /// <summary>
+    /// The complete URL to the media resource this recording is associated with.
+    /// </summary>
+    public string Media { get; set; }
+
+    /// <summary>
+    /// Name of associated media resource
+    /// </summary>
+    public string MediaName => Media.Split('/').Last();
+
+    /// <summary>
+    /// The state of the recording,
+    /// </summary>
+    public RecordingStates State { get; set; }
+  }
+
+  /// <summary>
+  ///   Query to get bridges
+  /// </summary>
+  public class RecordingQuery
+  {
+    /// <summary>
+    ///   Used for pagination to indicate the size of each page requested for querying a list of recordings. If no value is
+    ///   specified the default value is 25 (maximum value 1000).
+    /// </summary>
+    public int? Size { get; set; }
+  }
+
+  /// <summary>
+  /// States of recording
+  /// </summary>
+  public enum RecordingStates
+  {
+    /// <summary>
+    /// Recording is currently active.
+    /// </summary>
+    Recording,
+
+    /// <summary>
+    /// Recording complete and available for downloading or playing.
+    /// </summary>
+    Complete,
+
+    /// <summary>
+    /// Recording is complete but it is not available to download yet.
+    /// </summary>
+    Saving,
+
+    /// <summary>
+    /// Recording could not be uploaded
+    /// </summary>
+    Error
   }
 }
---
+++ test/Bandwidth.Net.Test/Api/RecordingTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Bandwidth.Net.Api;
+using LightMock;
+using Xunit;
+
+namespace Bandwidth.Net.Test.Api
+{
+  public class RecordingTest
+  {
+    [Fact]
+    public void TestList()
+    {
+      var response = new HttpResponseMessage
+      {
+        Content =
+          new JsonContent($"[{Helpers.GetJsonResourse("Recording")}]")
+      };
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidListRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(response));
+      var api = Helpers.GetClient(context).Recording;
+      var recordings = api.List();
+      ValidateRecording(recordings.First());
+    }
+
+    [Fact]
+    public async void TestGet()
+    {
+      var response = new HttpResponseMessage
+      {
+        Content = Helpers.GetJsonContent("Recording")
+      };
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidGetRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(response));
+      var api = Helpers.GetClient(context).Recording;
+      var recording = await api.GetAsync("id");
+      ValidateRecording(recording);
+    }
+
+    public static bool IsValidListRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Get && request.RequestUri.PathAndQuery == "/v1/users/userId/recordings";
+    }
+
+    public static bool IsValidGetRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Get && request.RequestUri.PathAndQuery == "/v1/users/userId/recordings/id";
+    }
+
+    private static void ValidateRecording(Recording item)
+    {
+      Assert.Equal("recordingId", item.Id);
+      Assert.Equal(RecordingStates.Complete, item.State);
+    }
+  }
+}
